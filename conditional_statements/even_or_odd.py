8f86d2ff7ef76bf1e065996ad6cb76419c059ab2
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-11 19:42:49 +0530

    conditional statements

---
+++ conditional_statements/even_or_odd.py
@@ -0,0 +1,16 @@
+# Problem 6 — Even or Odd
+# Take a number from the user and check whether it is:
+# Even
+# Odd
+
+num=int(input('enter a number:'))
+if num%2==0:
+    print('It is Even')
+else:
+    print('It is Odd')
+    
+
+
+#compressed version
+
+print("even" if num%2==0 else "odd")
