953b02b29289839e9c24d6e350365360c2255106
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-11 19:06:45 +0530

    Variables code

---
+++ variables.py
@@ -0,0 +1,16 @@
+# Problem 2 — Variables
+# Create a variable called name and store your name in it.
+
+name = 'Tendulkar'
+print('Hello',name)
+
+
+# Problem 3 — Input from User
+# Take a name from the user and print it.
+
+names=input('Enter Your Name:')
+print('Your name is:',names)
+
+# f-strings [interpolation]
+
+print(f'Your name is {names}')
