aed6045ef9b78989167a70b16c87dde0d08b550a
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-12 14:54:28 +0530

    reverse pyramid

---
+++ star_patterns/reverse_pyramid.py
@@ -0,0 +1,15 @@
+"""
+*********
+ *******
+  *****
+   ***
+    *
+
+"""
+
+for i in range(1,6):
+    for j in range(i-1):
+        print(' ',end='')
+    for k in range(2*(5-i)+1):
+        print('*',end='')
+    print()
