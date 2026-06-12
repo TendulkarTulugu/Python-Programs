819fa660926d38697400fc3ad57cbd8daed5bc1b
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-12 10:54:48 +0530

    Adding 2 more

---
+++ conditional_statements/Largest_of_two_numbers.py
@@ -0,0 +1,9 @@
+# Problem 8 — Largest of Two Numbers
+# Take two numbers from the user and print the larger number.
+
+n1=int(input('Enter your 1st number:'))
+n2=int(input('Enter your 2nd number:'))
+if n1>n2:
+    print('n1 is larger')
+else:
+    print('n2 is larger')
---
+++ conditional_statements/positive_or_negative.py
@@ -0,0 +1,13 @@
+# Problem 7 — Positive, Negative, or Zero
+# Take a number from the user and print:
+# "Positive"
+# "Negative"
+# "Zero"
+
+num=int(input('Enter Your number:'))
+if num>0:
+    print('Positive')
+elif num<0:
+    print('Negative')
+else:
+    print('Zero')
