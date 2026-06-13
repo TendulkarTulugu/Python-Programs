23edfea13c785b08a689b419ad3d623c7662294b
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-13 18:16:16 +0530

    diamond

---
+++ star_patterns/diamond.py
@@ -0,0 +1,27 @@
+"""
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+
+"""
+
+for i in range(1,6):
+    for j in range(5-i):
+        print(' ',end='')
+    for k in range(2*i-1):
+        print('*',end='')
+    print()
+for z in range(2,6):
+    for x in range(z-1):
+        print(' ',end='')
+    for y in range(2*(5-z)+1):
+        print('*',end='')
+    print()
+
+# it prints the diamond
