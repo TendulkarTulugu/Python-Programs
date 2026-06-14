c3d13e9e52f51d9f6bb6639399ee4e3089a3d0dc
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-14 15:01:07 +0530

    modifying

--- loops/step_value.py
+++ loops/step_value.py
@@ -6,6 +6,7 @@ for i in range(0,10,2):
 
 # it'll print as 0 2 4 6 8 upto 8 and excludes 10 bcz upper boundary.
 
+# --------------------------------
 
 # now i wanna print only odd numbers between 1,20. here i can go as
 
