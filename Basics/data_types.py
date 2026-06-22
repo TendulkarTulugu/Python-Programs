7ec0456349b7640407aac060f56d695d27815d3e
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-22 11:50:30 +0530

    Updating

--- Basics/data_types.py
+++ Basics/data_types.py
@@ -85,6 +85,58 @@ print(bool(-1))
 
 
 
+#==========================================================================================================================
+
+# String
+
+# single quotes
+a1='Hello'
+
+# double quotes
+a2="Tendul"
+
+# triple single quotes for multiple lines or paras
+a3=''' Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+It is used for:
+
+web development (server-side),
+software development,
+mathematics,
+system scripting.
+What can Python do?
+Python can be used on a server to create web applications.
+Python can be used alongside software to create workflows.
+Python can connect to database systems. It can also read and modify files.
+Python can be used to handle big data and perform complex mathematics.
+Python can be used for rapid prototyping, or for production-ready software development.'''
+
+
+
+# triple double quotes for multiple lines or paragraph
+a4="""Why Python?
+Python works on different platforms (Windows, Mac, Linux, Raspberry Pi, etc).
+Python has a simple syntax similar to the English language.
+Python has syntax that allows developers to write programs with fewer lines than some other programming languages.
+Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
+Python can be treated in a procedural way, an object-oriented way or a functional way.
+Good to know
+The most recent major version of Python is Python 3, which we shall be using in this tutorial.
+In this tutorial Python will be written in a text editor. It is possible to write Python in an Integrated Development Environment, such as Thonny,
+Pycharm, Netbeans or Eclipse which are particularly useful when managing larger collections of Python files."""
+
+print(type(a1))
+print(a1)
+print(type(a2))
+print(a2)
+print(type(a3))
+print(a3)
+print(type(a4))
+print(a4)
+
+
+
+
 
 
 
