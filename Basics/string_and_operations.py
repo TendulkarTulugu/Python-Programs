9b15d69695b9e081515b40a47227425b7601ec89
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-22 13:55:46 +0530

    Updating

---
+++ Basics/string_and_operations.py
@@ -0,0 +1,22 @@
+s='Tendulkar'
+
+# T   E  N  D  U  L  K  A  R
+# 0   1  2  3  4  5  6  7  8    -- POSITIVE INDEXING
+# -9 -8 -7 -6 -5 -4 -3 -2 -1    -- NEGATIVE INDEXING
+
+
+
+print(s)
+print(s[0])  # T
+
+print(s[-1]) # R
+
+print(s[2]) # N
+
+print(s[0:6]) # TENDUL
+
+print(s[:6]) # Tendul
+
+print(s[6:]) # kar
+
+print(s[::-1]) # reversing [ RAKLUDNET]
