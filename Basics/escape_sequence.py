8fca9d50736d6cd1aeb34778cd60f024a5c80221
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-22 11:51:10 +0530

    Updating

---
+++ Basics/escape_sequence.py
@@ -0,0 +1,18 @@
+# escape sequence
+
+# 1 Tab 
+Tab="Name:\tTendul"
+print(Tab)
+
+#2 New Line
+newl="Line1\nLine2"
+print(newl)
+
+# 3 To print text like file path simply to print single slash
+singles="C:\\Path\\folder"
+print(singles)
+
+# 4 Literal Quotes \"
+quot="she said\"HI\""
+print(quot)
+
