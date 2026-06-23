64478dd029f6b1eece7ee42f446d35b6b579ae60
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-23 14:02:35 +0530

    Updating

---
+++ conditional_statements/match_exp.py
@@ -0,0 +1,6 @@
+# mostly in different programming languages there is traditional switch-casse.
+
+# Python doesn't provide that traditional switch case. but we can achieve that similar behaviour.
+# In python 3.10 version and after that python will support that behaviour by using 'match'.
+
+
