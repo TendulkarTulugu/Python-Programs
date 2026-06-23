df46ae69b6dd183b76a277f2163d45d96aa67455
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-23 11:58:10 +0530

    Updating

---
+++ conditional_statements/if_else.py
@@ -0,0 +1,16 @@
+# conditional statements are mainly for the decision making.
+
+# If, If-Else, If-Else-If are the types
+
+
+# If is executed whenever the statement or specific condition is True otherwise it exectue and doesn't print anything.
+
+# If-Else are two seperate ones --> If executes when the specified condition is True
+# Else block executes if the specified condition is false.
+
+
+# if - else - if is simply 'elif' and it is used whenever there are multiple conditions are there.
+
+# let's see each one seperately
+
+
