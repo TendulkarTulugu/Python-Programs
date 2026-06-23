34c63389421737820562a366cdf104c113aaeb87
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-23 14:03:01 +0530

    Updating

--- conditional_statements/match_exp.py
+++ conditional_statements/match_exp.py
@@ -3,4 +3,22 @@
 # Python doesn't provide that traditional switch case. but we can achieve that similar behaviour.
 # In python 3.10 version and after that python will support that behaviour by using 'match'.
 
+day=5
+
+match day:
+    case 1:
+        print('Mon')
+    case 2:
+        print('Tue')
+    case 3:
+        print('Wed')
+    case 4:
+        print('Thu')
+    case 5:
+        print('Fri')
+    case 6:
+        print('Sat')
+    case 7:
+        print('Sun')
+
 
