ab61682ca89ee38c0d3bb3caa7c629add44bff74
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-24 14:03:58 +0530

    updating

--- loops/basic_for_loop.py
+++ loops/basic_for_loop.py
@@ -10,3 +10,14 @@ for i in range(5):
 
 #Starting: starting values
 # ending: it excludes the upper boundary. if end is 10, it excludes 10 and takes upto 9.
+
+
+# iterating over list
+
+data=['a','b','c','d']
+for d in data:
+    print(d)
+
+
+# iterate over string
+
