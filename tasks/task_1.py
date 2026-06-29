934723e08568f780956652097643aed658299c3e
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-29 12:05:33 +0530

    updating

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -84,7 +84,8 @@ print('--------------------')
 
 
 for i in range(1,6):
-    for j in range(1,i+1):
-        print(i,end='')
+    for j in range(i,0,-1):
+        print(j,end='')
     print()
-        
+
+print('-----------------')
