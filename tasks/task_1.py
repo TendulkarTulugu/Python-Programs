0f8e546c855a4f1060916420145f30605a52960a
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-29 14:24:58 +0530

    updating

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -199,5 +199,17 @@ for i in range(1,6):
     for j in range(6-i,6):
         print(j,end='')
     print()
-    
+print('-----------------')
 
+# another prblm
+
+# 12345
+# 2345
+# 345
+# 45
+# 5
+
+for i in range(1,6):
+    for j in range(i,6):
+        print(j,end='')
+    print()
