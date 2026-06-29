79a4b0d23ac7db099c30354981faeac89db4a9f3
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-29 14:03:44 +0530

    updating

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -135,3 +135,17 @@ for i in range(1,5):
 print('--------------------')
 
 
+# Bonus  Continous alphabets
+
+# A
+# BC
+# DEF
+# GHIJ
+# KLMNO
+
+a=65
+for i in range(1,6):
+    for j in range(i):
+        print(chr(a),end='')
+        a+=1
+    print()
