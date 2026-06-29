686a0cb4b87eec4617ce00212a2c3b7c235f4d1e
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-29 11:50:25 +0530

    updating

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -10,3 +10,19 @@
 
 # 1 pyramid
 
+#     *
+#    ***
+#   *****
+#  *******
+# *********
+
+
+
+for i in range(1,6):
+    for j in range(5-i):
+        print(' ',end='')
+    for k in range(2*i-1):
+        print('*',end='')
+    print()
+
+print('------------------')
