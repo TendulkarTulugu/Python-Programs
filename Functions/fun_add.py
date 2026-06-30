0d5adb7974bcb08510e6cec9b8c3e685db1ca0f1
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-30 16:11:12 +0530

    updating

--- Functions/fun_add.py
+++ Functions/fun_add.py
@@ -14,3 +14,15 @@ def add(a,b):
     
 add(10,20)
 add(5,7)
+
+
+
+# There are two ways a function can give you an answer.
+
+# Method 1: print() -------- This displays the answer.
+
+
+# Method 2: return -------- This returns the answer to wherever the function was called.
+
+# This is one of the biggest concepts in Python.
+
