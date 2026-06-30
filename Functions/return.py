a9db21157f64157aee18b233ad2c36c791ab31d3
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-30 16:16:05 +0530

    updating

---
+++ Functions/return.py
@@ -0,0 +1,21 @@
+# Your Next Challenge
+
+# Instead of using print(), use return.
+
+
+def add(a,b):
+    return a+b
+
+result=add(10,20)
+print(result)
+
+'''
+Simple Rule to Remember
+print()
+
+Shows the value on the screen.
+
+return
+
+Sends the value back to the caller.
+'''
