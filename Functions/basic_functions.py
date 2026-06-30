dea70b1e7a9c228c1a2ff0b4a5f3278cfc440863
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-30 11:29:28 +0530

    functions

---
+++ Functions/basic_functions.py
@@ -0,0 +1,94 @@
+'''
+What is a Function?
+
+A function is a block of code that performs a specific task.
+
+Imagine you have to greet 100 people.
+
+Without a function:
+
+# print("Hello")
+# print("Hello")
+# print("Hello")
+# print("Hello")
+
+its hard to print 100 lines using print 100 lines
+
+loops are useful to run 100 times but it executes everytime whenever the code runs
+
+functions are useful to print the output whenever the function is called
+
+
+syntax:
+
+def function_name():
+    statments
+
+
+calling a function
+
+function_name()
+
+
+'''
+
+
+
+# declaration 
+
+
+def greet():
+    print("Hello")
+greet()
+
+
+
+
+'''
+def is the keyword used to defining a function.
+
+function_name can be anything meaningful. Like
+
+def reverse():
+
+def sub():
+
+def prime()
+
+
+
+parentheses -- ()
+
+These parentheses are required.
+
+We'll soon learn to pass values inside them.
+
+'''
+
+
+
+'''
+
+Your First Task
+
+Write a function that prints:
+
+Welcome to Coding
+
+
+
+Requirements:
+
+Create a function named welcome
+Call it 3 times
+
+Don't worry about parameters yet.
+
+
+'''
+
+def welcome():
+    print('Welcome to Coding')
+welcome()
+welcome()
+welcome()
