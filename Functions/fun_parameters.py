0586350c3ea3d025e1044605acf2e5c60d7a46fc
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-06-30 16:01:59 +0530

    functions

---
+++ Functions/fun_parameters.py
@@ -0,0 +1,40 @@
+'''
+until before we are printing 5x5 stars
+
+what if  i want 
+
+***
+***
+***
+
+or
+
+********
+********
+********
+********
+********
+********
+********
+********
+
+her we don't want to create function everytime.
+
+Insted we can pass a value into the function.
+
+Let's work on this
+'''
+
+
+
+
+def square(n):
+    for i in range(n):
+        print('*'*n)
+
+square(5)
+
+
+
+
+
