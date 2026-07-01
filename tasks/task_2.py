0ad16df615c2dc06b74f29f136462210d0ed81b7
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-07-01 11:23:32 +0530

    tasks

---
+++ tasks/task_2.py
@@ -0,0 +1,30 @@
+# the task is of about the functions
+
+'''
+🎯 Homework (5 Function Problems)
+
+Try these without looking at today's code.
+
+1. Sum of two numbers
+
+Returns the sum.
+
+2. Find the smaller number
+
+Returns the smaller value.
+
+
+3. Square of a number
+
+
+4. Check palindrome number
+
+Returns "Palindrome" or "Not Palindrome".
+
+
+5. Prime number
+
+Returns "Prime" or "Not Prime".
+
+'''
+
