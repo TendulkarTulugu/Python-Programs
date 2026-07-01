fdfb6338c4e82318ac80c56c357f8b153280b8b9
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-07-01 11:55:18 +0530

    tasks

---
+++ Functions/func_vowels.py
@@ -0,0 +1,23 @@
+'''
+🎯 Next Mission
+
+We're now going to combine functions + strings.
+
+Challenge 1
+
+count number if vowels in the word
+
+'''
+
+
+def count_vowels(text):
+    vowels='aeiouAEIOU'
+    count=0
+    for ch in text:
+        if ch in vowels:
+            count+=1
+    return count
+result=count_vowels('Education')
+print(result)
+
+
