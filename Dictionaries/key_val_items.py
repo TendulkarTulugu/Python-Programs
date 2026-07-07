2907a1c6beb32146f3e3c19f6d17bbd62784897b
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-07-07 12:42:10 +0530

    Dictionary

--- Dictionaries/key_val_items.py
+++ Dictionaries/key_val_items.py
@@ -23,3 +23,66 @@ for key,value in student.items():
     print(key,':',value)
 
 # But items() lets us get both the key and value directly.
+
+
+'''
+🎯 Your Turn
+
+Create this dictionary:
+
+student = {
+    "name": "Tendul",
+    "age": 23,
+    "branch": "AI & DS",
+    "cgpa": 8.04
+}
+
+Now write three loops:
+
+Loop 1
+
+Print only the keys.
+
+Expected:
+
+name
+age
+branch
+cgpa
+Loop 2
+
+Print only the values.
+
+Expected:
+
+Tendul
+23
+AI & DS
+8.04
+Loop 3
+
+Print both.
+
+Expected:
+
+name : Tendul
+age : 23
+branch : AI & DS
+cgpa : 8.04
+
+'''
+students = {
+    "name": "Tendul",
+    "age": 23,
+    "branch": "AI & DS",
+    "cgpa": 8.04
+}
+
+for key in students:
+    print(key)
+
+for values in students:
+    print(students[values])
+
+for key,value in students.items():
+    print(key,':',value)
