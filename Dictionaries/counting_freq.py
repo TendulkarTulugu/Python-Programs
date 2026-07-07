8ec9005b517260423b0d1c448d92fe0bfafde3c3
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-07-07 12:01:00 +0530

    Dictionary

---
+++ Dictionaries/counting_freq.py
@@ -0,0 +1,174 @@
+#5
+
+'''
+🎯 Problem: Frequency of Elements
+
+Remember this?
+
+numbers = [1, 2, 1, 3, 2, 1]
+
+before we wrote:
+
+count_1 = 0
+count_2 = 0
+count_3 = 0
+
+It worked...
+
+But only for 1, 2, and 3.
+
+🤔 Think About This
+
+Suppose I give you:
+
+numbers = [10, 20, 10, 50, 30, 20, 10]
+
+Can you write:
+
+count_10
+count_20
+count_30
+count_50
+
+Yes.
+
+Now suppose I give you 1000 different numbers.
+
+😅 Impossible.
+
+⭐ Dictionary to the Rescue
+
+Instead of this:
+
+count_1 = 3
+count_2 = 2
+count_3 = 1
+
+We'll store everything in one dictionary.
+
+Like this:
+
+frequency = {
+    1: 3,
+    2: 2,
+    3: 1
+}
+
+See how neat it is?
+
+🧠 The Logic
+
+Let's solve it together.
+
+Initially:
+
+frequency = {}
+
+This means:
+
+{}
+
+An empty dictionary.
+
+Read the first number
+1
+
+Is 1 already a key in the dictionary?
+
+No.
+
+So create it.
+
+frequency[1] = 1
+
+Dictionary becomes:
+
+{
+    1: 1
+}
+Read the second number
+2
+
+Already present?
+
+No.
+
+Create it.
+
+frequency[2] = 1
+
+Now:
+
+{
+    1: 1,
+    2: 1
+}
+Read the third number
+1
+
+Already present?
+
+✅ Yes.
+
+So instead of creating it again:
+
+Increase its count.
+
+frequency[1] += 1
+
+Now:
+
+{
+    1: 2,
+    2: 1
+}
+
+Continue...
+
+Eventually we get:
+
+{
+    1: 3,
+    2: 2,
+    3: 1
+}
+🎯 Your Challenge
+
+Write this function:
+
+def frequency(nums):
+Rules
+
+Use:
+
+frequency = {}
+
+Think about this question:
+
+How do I check whether a key already exists in a dictionary?
+
+💡 Hint:
+
+Just like we checked:
+
+if ch in vowels:
+
+Can we check:
+
+if num in frequency:
+
+🤔
+'''
+
+def count_freq(nums):
+    frequency={}
+    for i in nums:
+        if i not in frequency:
+            frequency[i]=1
+        elif i in frequency:
+            frequency[i]+=1
+    return frequency
+
+numbers = [10, 20, 10, 50, 30, 20, 10]
+
+print(count_freq(numbers))
