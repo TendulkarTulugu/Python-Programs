e3a6b31e8413fd63a65c00e15ea86a46baad2f59
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-07-07 12:35:22 +0530

    Dictionary

--- Dictionaries/key_val_items.py
+++ Dictionaries/key_val_items.py
@@ -1,2 +1,25 @@
 #6
 
+student = {
+    "name": "Tendul",
+    "age": 23,
+    "branch": "AI & DS"
+}
+
+print(student.keys())
+# it'll give all the keys of dict
+
+print(student.values())
+#it'll give all the values of dict
+
+print(student.items())
+# it 'll give all items. Each item is a (key, value) pair.
+
+for key in student:
+    print(key,':',student[key])
+# it give the dict key:values
+
+for key,value in student.items():
+    print(key,':',value)
+
+# But items() lets us get both the key and value directly.
