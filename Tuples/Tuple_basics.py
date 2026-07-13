cf1e461a6c5cd7bcb1c3c1dd611ce020551344bb
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-07-13 11:33:56 +0530

    Tuples

--- Tuples/Tuple_basics.py
+++ Tuples/Tuple_basics.py
@@ -71,3 +71,62 @@ print(student[2])
 # student[1]=24
 # it shows the error as the tuple is immutable
 print(student)
+
+
+
+'''
+⭐ Why Do Tuples Exist?
+
+This is the question most beginners ask.
+
+"If lists can do more, why use tuples?"
+
+Good question.
+
+Imagine your date of birth.
+
+22-08-2003
+
+Should your program accidentally change it?
+
+❌ No.
+
+Or GPS coordinates:
+
+(17.3850, 78.4867)
+
+They shouldn't change either.
+
+
+# So we store data that should remain fixed in tuples.
+'''
+
+
+nums=(10,20,30,30,40)
+print(len(nums))
+print(nums[2])
+print(nums)
+
+for num in nums:
+    print(num)
+
+
+'''
+Many people think:
+
+"Tuple is immutable, so maybe we can't loop over it."
+
+But looping doesn't modify the tuple.
+
+You're only reading its values.
+
+Reading is allowed.
+
+Changing is not.
+
+Think of a tuple like a book in a library:
+
+✅ You can read every page.
+❌ You cannot rewrite the pages.
+
+'''
