8cd29e754111d6a2f9fc64a62e9c71d5d2df5629
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-07-14 11:32:24 +0530

    File Handling

---
+++ File Handling/file_read.py
@@ -0,0 +1,60 @@
+#2
+
+# read() 
+
+file=open(r"File Handling\sample.txt")
+content=file.read()
+print(content)
+file.close()
+
+print('---readline()---')
+
+# readline()
+#  It reads only the first line.
+
+file = open(r"File Handling\sample.txt")
+print(file.readline())
+file.close()
+
+print('--------')
+
+file = open(r"File Handling\sample.txt")
+print(file.readline())
+print(file.readline())
+file.close()
+
+'''
+
+calling readline()- 2 times
+
+Each call reads the next line.
+
+Think of it like turning pages in a book.
+'''
+
+
+# readlines()
+
+print('---readlines()---')
+
+file = open(r"File Handling\sample.txt")
+
+lines = file.readlines()
+
+print(lines)
+
+file.close()
+
+
+'''
+['Hello Tendul\n',
+ 'Welcome to Python\n',
+ 'Today is a good day.']
+ 
+
+Notice the \n?
+
+It represents a newline character.
+ 
+'''
+
