6ea41afd37de9c53f8d07f07f504d7f24fdaa768
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-07-14 11:25:37 +0530

    File Handling

--- File Handling/basics.py
+++ File Handling/basics.py
@@ -86,3 +86,69 @@ Close it 📕
 
 Exactly the same idea.
 '''
+import os
+
+print("Current Working Directory:", os.getcwd())
+print("Files in current folder:", os.listdir())
+
+# # file=open("sample.txt") -- it doesn't retrive the output as the path is not correct.
+
+# file = open(r"D:\OneDrive\Desktop\Python\Python\File Handling\sample.txt") -- it can retrive the data.
+# print(file.read()) -- it reads the data
+# file.close() --- it closes the file.
+
+'''
+here i have a folder named python.
+
+in that folder i have multiple folders like basics,loops,etc.. file handling too.
+
+as now i was working in file handling folder i created the sample.txt in file handling folder.
+
+but the path is D:\OneDrive\Desktop\Python\Python> upto there.
+
+so it doesn't access the file. so it throws error.
+
+file=open(r'File Handling\sample.txt') 
+
+i used this to retive the data from another folder from the current path.
+
+'''
+
+
+
+file=open(r'File Handling\sample.txt') # it is the best way to retrive
+print(file.read())
+file.close()
+
+
+# there is another way to work the same
+
+import os
+
+current_folder = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_folder, "sample.txt")
+
+file = open(file_path)
+print(file.read())
+file.close()
+
+'''
+🎯 One More Tip
+
+Instead of:
+
+file = open(...)
+print(file.read())
+file.close()
+
+Modern Python prefers:
+
+with open(r"File Handling\sample.txt") as file:
+    print(file.read())
+
+Why?
+
+Because with automatically closes the file, even if an error occurs.
+
+We'll learn with properly after you understand open(), read(), write(), and append().
+'''
