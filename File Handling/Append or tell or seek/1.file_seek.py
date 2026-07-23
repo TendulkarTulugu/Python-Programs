aa8df7e3335f392c6a00e8bb39c74d407ee9bdb4
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-07-23 11:06:17 +0530

    File Handling Append Seek Tell

--- File Handling/Append or tell or seek/1.file_seek.py
+++ File Handling/Append or tell or seek/1.file_seek.py
@@ -64,3 +64,58 @@ file.seek(7) #-- Cursor moves to the 7th position
 print(file.read()) #-- Reads from the current cursor upto the end.
 
 file.close() #-- closes the file.
+
+
+file = open("File Handling\Append or tell or seek\sample.txt")
+
+print(file.readline())
+
+file.seek(0)
+
+print(file.readline())
+
+file.close()
+
+#------------------------
+
+file = open("File Handling\Append or tell or seek\sample.txt")
+
+data = file.readlines()
+for name in data:
+    print(name.strip())
+print(len(data))
+print(type(data))
+file.close()
+
+
+'''
+Now let me ask you a few rapid-fire questions like an interviewer.
+
+Q1
+
+Which function returns a list?
+
+Q2
+
+Which function reads only one line?
+
+Q3
+
+Which mode deletes existing data?
+
+Q4
+
+Which mode adds data without deleting existing content?
+
+Q5
+
+What does tell() return?
+
+Q6
+
+What does seek(0) do?
+
+Q7
+
+What happens if you open a file in "a" mode and the file doesn't exist?
+'''
