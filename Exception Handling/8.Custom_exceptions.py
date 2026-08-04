80a6309dfab160943730287b5ff4cf123b48d773
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-08-04 10:52:21 +0530

    custom_exception

--- Exception Handling/8.Custom_exceptions.py
+++ Exception Handling/8.Custom_exceptions.py
@@ -33,3 +33,69 @@ instead of using ValueError?
 That's exactly what Custom Exceptions are for.
 
 '''
+
+
+'''
+Suppose you're building an ATM application.
+
+balance = 500
+withdraw = 1000
+
+What happens?
+
+Obviously,
+
+Insufficient Balance
+
+Now tell me...
+
+Which built-in exception should we use?
+
+ValueError ❌
+ZeroDivisionError ❌
+TypeError ❌
+IndexError ❌
+
+None of them describe the actual problem.
+
+This isn't a Python error.
+
+It's our business rule.
+
+Real Life Analogy
+
+Imagine a hospital.
+
+Python already has doctors for:
+
+ValueError
+TypeError
+IndexError
+KeyError
+ZeroDivisionError
+
+Now suppose a patient comes with:
+
+Alien Virus
+
+Does any doctor specialize in that?
+
+❌ No.
+
+So the hospital creates a new department.
+
+Similarly,
+
+Python allows us to create our own exception class.
+
+First Custom Exception
+class InsufficientBalanceError(Exception):
+    pass
+
+Don't worry about the syntax.
+
+Let's understand it word by word.
+'''
+
+# wait to understand this we need to complete OOP so we'll complete it later.
+
