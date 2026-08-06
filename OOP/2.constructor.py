686624031494c171547b5af7642d7e9fb4ae5c35
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-08-06 11:05:50 +0530

    Constructor

--- OOP/2.constructor.py
+++ OOP/2.constructor.py
@@ -21,3 +21,206 @@ rahul.course
 
 This is where OOP starts to become really powerful.
 '''
+
+
+
+class Student:
+    pass
+
+rahul = Student()       #creating the object.
+
+print(rahul)   
+
+
+'''
+Does Python know that:
+
+Name = Rahul
+Age = 20
+Course = Python
+'''
+
+# No, because we've only created an empty object. We haven't given it any data yet.
+
+
+'''
+We only said:
+
+"Create one Student object."
+
+We never said:
+
+Name = Rahul
+Age = 20
+Course = Python
+
+So Python creates something like this:
+
+rahul
+├── Name   → ❓
+├── Age    → ❓
+└── Course → ❓
+
+Everything is empty because we never initialized it.
+
+Then how do we give values?
+
+This is exactly why Python introduced constructors.
+
+Instead of:
+
+rahul = Student()
+
+we'll soon write:
+
+rahul = Student("Rahul", 20, "Python")
+
+Now Python immediately knows:
+
+rahul
+├── Name   → Rahul
+├── Age    → 20
+└── Course → Python
+'''
+
+# from now lets learn about the constructor.
+
+'''
+Imagine this situation.
+
+Suppose I give you a blank student form.
+
+Student Form
+
+Name : __________
+
+Age : __________
+
+Course : __________
+
+It's empty.
+
+Now Rahul comes and fills it.
+
+Student Form
+
+Name : Rahul
+
+Age : 20
+
+Course : Python
+
+Now Priya comes.
+
+Student Form
+
+Name : Priya
+
+Age : 21
+
+Course : Java
+
+Notice something.
+
+The form never changes.
+
+Only the values change.
+
+Now think about Python.
+
+We have:
+
+rahul = Student()
+
+But we never gave Rahul's information.
+
+Wouldn't it be better if we could write:
+
+rahul = Student("Rahul", 20, "Python")
+
+so that at the moment the object is created, Python also receives all the required information?
+
+🤔
+
+'''
+
+
+
+
+
+
+'''
+Think about what happens.
+
+When Python sees:
+
+rahul = Student("Rahul", 20, "Python")
+
+Python thinks:
+
+"Okay, create a new Student object."
+
+Then:
+
+"Now I have three pieces of information."
+
+Name   → Rahul
+Age    → 20
+Course → Python
+
+Then Python stores them inside the object.
+
+So after creation, the object looks like:
+
+rahul
+├── Name   → Rahul
+├── Age    → 20
+└── Course → Python
+
+Exactly what you said.
+
+But here's the next question...
+
+How does Python know that:
+
+"Rahul"
+
+should go into Name?
+
+How does Python know that:
+
+20
+
+should go into Age?
+
+How does Python know that:
+
+"Python"
+
+should go into Course?
+
+It doesn't.
+
+We have to teach Python.
+
+And that's exactly what a constructor does.
+
+Meet the Constructor
+'''
+
+
+class Student:
+
+    def __init__(self, name, age, course):
+        print(name)
+        print(age)
+        print(course)
+
+'''
+Don't panic. 😄
+
+I'm not explaining it yet.
+
+We're going to predict it.
+
+'''
