8765e2973a5e5ef2e830899994e09361ff98816f
Tendulkar Tulugu <tendulkartulugu117@gmail.com>
2026-09-08 10:40:08 +0530

    Updating

---
+++ Basics/operators.py
@@ -0,0 +1,32 @@
+'''
+1. arithmetic---> +,-,*,/,//,%,**
+2. assignment---> =,+=,-=,*=,/=,:=
+3 comparison---> ==,!=,>,<,>=,<=
+4. logical-->And, or , Not
+5. Identity--->is, is not
+6. membership---> in, not in
+7. bitwise --> &,|,^,~,<<,>>
+'''
+
+
+# BODMAS
+(2+3)+5-6*8+2-6
+
+
+# Operator precedence
+
+'''
+1. ()
+2. **
+3. +x,-x,~x
+4. *,/,//,%
+5. +,-
+6. <<,>>
+7. &
+8. ^
+9. |
+10. ==,!=
+'''
+
+
+
